3727dbad20eb9e0c825bbd5d31e32286abcaf9a9
Sachin Pal <sachinpal01012003@gmail.com>
2023-04-26 01:10:32 +0530

    Added Changes to Page

--- src/pages/FrameSeven/index.jsx
+++ src/pages/FrameSeven/index.jsx
@@ -1,44 +1,38 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-import { Text} from "components";
+import { Text } from "components";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const FrameSevenPage = ({data, onChange}) => {
+const FrameSevenPage = ({ data, onChange }) => {
   const navigate = useNavigate();
   const [rank, setRank] = useState();
 
   const handleRankChange = (event) => {
-    setRank(event.target.value)
+    setRank(event.target.value);
   };
 
-  const handleKeyDown = (event) => {
-
-    if (event.key === 'Enter') {
-
-      const newData = {...data, 'Opening Rank': rank}
-      onChange(newData)
+  const handleKeyDown = async (event) => {
+    if (event.key === "Enter") {
+      const newData = { ...data, "Opening Rank": rank };
+      onChange(newData);
 
       // Make API post request here using fetch or axios
-      fetch('http://127.0.0.1:5000', {
-        method: 'POST',
-        body: JSON.stringify(newData),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-        navigate('/framesix');
-      })
-      .catch(error => {
-        console.error('There was a problem with the API request:', error);
-      });
+      await axios
+        .post("http://127.0.0.1:5000", newData, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+          crossDomain: true
+        })
+        .then((response) => {
+          console.log(response.data);
+          navigate("/framesix");
+        })
+        .catch((error) => {
+          console.error("There was a problem with the API request:", error);
+        });
     }
   };
 
@@ -53,13 +47,13 @@ const FrameSevenPage = ({data, onChange}) => {
           Enter your All India Rank!!
         </Text>
         <input
-        className="common-pointer cursor-pointer font-normal leading-[normal] mb-[166px] min-w-[430px] sm:min-w-full not-italic sm:text-4xl md:text-[38px] text-[40px] text-center text-gray_300 w-auto"
-        type="number"
-        value={rank}
-        onChange={handleRankChange}
-        onKeyDown={handleKeyDown}
-        placeholder="Type here..."
-      />
+          className="common-pointer cursor-pointer font-normal leading-[normal] mb-[166px] min-w-[430px] sm:min-w-full not-italic sm:text-4xl md:text-[38px] text-[40px] text-center text-gray_300 w-auto"
+          type="number"
+          value={rank}
+          onChange={handleRankChange}
+          onKeyDown={handleKeyDown}
+          placeholder="Type here..."
+        />
       </div>
     </>
   );
