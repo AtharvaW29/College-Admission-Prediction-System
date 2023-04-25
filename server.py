fb1e6083d8dd8a145c8c4080a508020ec5cce29e
Sachin Pal <sachinpal01012003@gmail.com>
2023-04-26 01:15:30 +0530

    Made Requirement files

--- server.py
+++ server.py
@@ -3,11 +3,21 @@ import numpy as np
 import json
 import sys
 from model_generator import get_predictions
+from flask_cors import CORS
 
 flask_app = Flask(__name__)
 
+CORS(flask_app, resources={r"/*": {"origins": "http://localhost:3000"}})
+
 # # loading the save model
 # classifier = joblib.load('classifier.joblib')
+
+@flask_app.after_request
+def add_cors_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
 
 # route to get the predictions
 @flask_app.route("/",methods=["POST"])
